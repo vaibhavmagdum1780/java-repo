255e9408f11601d4c76542f80815de7495114c69
Vaibhav Magdum <vmagdum51@gmail.com>
2026-01-14 07:44:24 +0530

    Initial commit

---
+++ java/DailyCodes/Program.java
@@ -0,0 +1,19 @@
+
+
+
+
+
+import java.lang.*;
+import java.io.*;
+
+class IODemo{
+   	public static void main(String[] args)throws IOException {
+	
+		System.out.println("Hello World");
+
+		int ch =System.in.read();
+		System.out.println(ch);
+	
+	}
+
+}
